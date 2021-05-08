91c93a3077da5cda913972a36acfd3c9fc9c297d
Mario23junior <mariojunior3251@gmail.com>
2021-05-08 04:50:10 -0300

    Iniciando projeto de consumo de api NASA MarsRover

---
+++ src/main/java/com/project/mars/marsRover/MarsRoverApplication.java
@@ -0,0 +1,13 @@
+package com.project.mars.marsRover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mars/marsRover/MarsRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mars.marsRover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
