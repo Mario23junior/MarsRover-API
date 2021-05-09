81afa1326480b5d133d9e1ce83f6f20d3151694f
Mario23junior <mariojunior3251@gmail.com>
2021-05-09 18:42:53 -0300

    Listando informacaoes por pagina

--- src/main/java/com/project/mars/marsRover/Service/MarsRoverApi.java
+++ src/main/java/com/project/mars/marsRover/Service/MarsRoverApi.java
@@ -11,10 +11,10 @@ public class MarsRoverApi {
 	
 	 private String keyApi = "O9GhnLpLgZPJ6ZvdMEaDtqXZUmnbdVC1tlf9SG55";
  	
- 	 public MarsRoverApiResponse Datalist(Integer marsSol , String roverType, String camera) {
+ 	 public MarsRoverApiResponse Datalist(Integer marsSol , String roverType, String camera, Integer page) {
 		RestTemplate restTemplate = new RestTemplate();
 		
-		String urlApi = "https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverType+"/photos?"+camera+"&sol="+marsSol+"&api_key="+keyApi;
+		String urlApi = "https://api.nasa.gov/mars-photos/api/v1/rovers/"+roverType+"/photos?"+camera+"&sol="+marsSol+"&page="+page+"&api_key="+keyApi;
 		
 		ResponseEntity<MarsRoverApiResponse> response = restTemplate.getForEntity(urlApi,MarsRoverApiResponse.class);
 		return(response.getBody());		
